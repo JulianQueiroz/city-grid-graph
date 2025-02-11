15d9c582c7e813daf18ae48781711329601d5f00
juliana <julianatko@gmail.com>
2025-02-10 21:04:03 -0300

    👌 IMPROVE: code update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from operator import add
 from itertools import combinations
 
+
 def DFS(matrix, x, y, visited, target):
     if x < 0 or x >= matrix.shape[0] or y < 0 or y >= matrix.shape[1]:
         return
@@ -16,8 +17,9 @@ def DFS(matrix, x, y, visited, target):
     DFS(matrix, x, y + 1, visited, target)
     DFS(matrix, x, y - 1, visited, target)
 
+
 def count_groups(matrix, look_for) -> list:
-    """counts orthogonally connected elements as groups."""
+    """counts equal elements in the orthogonal as groups."""
     visited = set()
     all_groups = []
     for i in range(matrix.shape[0]):
@@ -29,8 +31,9 @@ def count_groups(matrix, look_for) -> list:
                 visited.update(new_group)
     return all_groups
 
+
 def update_groups_id(matrix, groups, node_type) -> set:
-    """assigns unique IDs to groups or individual elements."""
+    """makes groups, or individuals, have unique IDs."""
     all_groups_id = set()
     for i, group in enumerate(groups, start=1):
         for position in group:
@@ -39,9 +42,11 @@ def update_groups_id(matrix, groups, node_type) -> set:
             all_groups_id.add(group_id)
     return all_groups_id
 
+
 def look_for_road_connection(matrix, groups, road_intersection_id) -> dict:
-    """iterates over the position of each road group, checking its neighbors
-    and identifying buildings/warehouses that the road connects to."""
+    """iterates over the position of each Road group, checking
+its adjacent ones and getting the Buildings/Warehouses that this Road
+connects to."""
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     road_connections = {}
     for group in groups:
@@ -56,30 +61,32 @@ def look_for_road_connection(matrix, groups, road_intersection_id) -> dict:
             road_connections.setdefault(matrix[position], set()).update(connected_entities)
     return road_connections
 
+
 def lookup_road_connections_deadends_intersections(road_one_connections, road_two_connections) -> list:
-    """categorizes connections through roads, intersections, and dead ends."""
+    """separates and categorizes connections through Roads, Intersections
+and which of them are Dead ends."""
     road_intersections, road_dead_ends, road_direct_connections = [], set(), {}
-    # roads with width 1
+    # roads of width 1.
     for road_id, road_connections in road_one_connections.items():
         build_connections = [build_id for build_id in road_connections if not build_id.startswith("E")]
         intersections = [rid for rid in road_connections if rid not in build_connections]
-        # direct connections passing through only one road
+        # direct connections, pass through only one road.
         if len(build_connections) > 1:
             for combination in combinations(build_connections, 2):
                 road_direct_connections.setdefault(road_id, set()).update([combination])
-        else:  # dead-end road
-            # road connects to only one building or warehouse
+        else:  # dead end street.
+           # road connects to only one Building or Warehouse.
             if len(build_connections) == 1:
                 road_dead_ends.add(f'{road_id}-{build_connections[0]}')
             else:
                 road_dead_ends.add(road_id)
-        # intersections passing through more than one road
+        # intersections, pass through more than one Road.
         for rid in intersections:
             build_connections = [build_id for build_id in road_two_connections[rid] if not build_id.startswith("E")]
             if len(build_connections) > 1:
                 road_intersections.append(f"{road_id}-{rid}")
             else:
-                # road connects to only one building or warehouse
+                # road connects to only one Building or Warehouse.
                 if len(build_connections) == 1:
                     road_dead_ends.add(f'{rid}-{build_connections[0]}')
                 else:
@@ -98,38 +105,100 @@ def lookup_road_connections_deadends_intersections(road_one_connections, road_tw
                 road_connections.setdefault(road_ids, set()).update([combination])
     return road_connections, road_direct_connections, road_dead_ends
 
+
 def build_graphviz_graph(building_ids, warehouse_ids, road_intersections, road_direct_connections, road_dead_ends):
-    """generates a graph in Graphviz format."""
     with open("result.dot", "w", encoding="utf-8") as file:
         file.write("graph Result {\n")
         file.write("\tnode [shape=circle, style=filled, color=lightblue];\n")
         file.write("\tedge [color=gray];\n")
-        # create nodes for buildings
+
+        # creates the Nodes for the Buildings.
         for build_id in building_ids:
-            file.write(f'\t{build_id} [label="Building-{build_id}"];\n')
-        # create nodes for warehouses
+            file.write(f'\t{build_id} [label="Prédio-{build_id}"];\n')
+
+       # creates the Nodes for the Warehouses.
         for warehouse_id in warehouse_ids:
-            file.write(f'\t{warehouse_id} [label="Warehouse-{warehouse_id}"];\n')
-        # create nodes for road intersections
+            file.write(f'\t{warehouse_id} [label="Armazém-{warehouse_id}"];\n')
+
+        # creates Road Intersection Nodes.
         for road_id in road_intersections.keys():
-            file.write(f'\t"{road_id}" [label="Road Intersection"];\n')
+            file.write(f'\t"{road_id}" [label="Interseção de Estrada"];\n')
+
+        # creates Road End Nodes.
+        for road_id in road_dead_ends:
+            file.write(f'\t"{road_id}" [label="Término de Estrada"];\n')
+            if '-' in road_id:
+                origin, destiny = road_id.split('-')
+               # road Width. (origin is always the Road)
+                road_size = 1 if origin.startswith("E1") else 2
+                file.write(f'\t"{road_id}" -- {destiny} [label="{road_size}"];\n')
+
+        # creates the certices of direct connections (only one road).
+        for road_id, all_connections in road_direct_connections.items():
+            for connection in all_connections:
+                origin, destiny = connection
+                # road width.
+                road_size = 1 if road_id.startswith("E1") else 2
+                file.write(f'\t{origin} -- {destiny} [label="{road_size}"];\n')
+
+        # creates the vertices of connections by intersection (more than one road).
+        written_edges = set()
+        for road_id, all_connections in road_intersections.items():
+            for connection in all_connections:
+                origin, destiny = connection
+                road_id_a, road_id_b = road_id.split("-")
+                # road width.
+                road_size_a = 1 if road_id_a.startswith("E1") else 2
+                road_size_b = 1 if road_id_b.startswith("E1") else 2
+                # prevents edge repetition
+                edge_origin_intersection = (origin, road_id)
+                if edge_origin_intersection not in written_edges:
+                    file.write(f'\t{origin} -- "{road_id}" [label="{road_size_a}"];\n')
+                    written_edges.add(edge_origin_intersection)
+                edge_intersection_destiny = (road_id, destiny)
+                if edge_intersection_destiny not in written_edges:
+                    file.write(f'\t"{road_id}" -- {destiny} [label="{road_size_b}"];\n')
+                    written_edges.add(edge_intersection_destiny)
+
         file.write("}")
-    print('Graphviz code saved as "result.dot"')
+        file.close()
+    print('graphviz code saved in "result.txt"')
+
 
 def main() -> None:
-    """main method."""
+    """Main method."""
+    # creates an NxM matrix with random values ​​from 1 to 4, where:
+    # 1 -> represents a Building.
+    # 2 -> represents a Warehouse (special type of Building).
+    # 3 -> represents a Road ONE tile wide.
+    # 4 -> represents a Road TWO tiles wide.
+    # 5 -> represents an Empty space.
     matrix = np.random.randint(1, 5, size=(10, 10)).astype(object)
-    building_ids = update_groups_id(matrix, count_groups(matrix, 1), "P")
-    warehouse_ids = update_groups_id(matrix, count_groups(matrix, 2), "A")
+   # get the position of all Buildings that form groups in the orthogonal.
+    # update the IDs of these Buildings to unique IDs.
+    building_groups = count_groups(matrix, 1)
+    building_ids = update_groups_id(matrix, building_groups, "P")
+    # gets the position of all Warehouses that form groups in the orthogonal.
+    # updates the IDs of these Warehouses to unique IDs.
+    warehouse_groups = count_groups(matrix, 2)
+    warehouse_ids = update_groups_id(matrix, warehouse_groups, "A")
+    # empty Spaces, just change the format (to string) and group them in the grid.
     _ = update_groups_id(matrix, count_groups(matrix, 5), "~")
+    # get the position of all Roads that form groups in the orthogonal.
+    # update the IDs of these Roads to unique IDs.
     road_one_groups = count_groups(matrix, 3)
     road_two_groups = count_groups(matrix, 4)
     _ = update_groups_id(matrix, road_one_groups, "E1")
     _ = update_groups_id(matrix, road_two_groups, "E2")
-    road_intersections, road_direct_connections, road_dead_ends = lookup_road_connections_deadends_intersections(
-        look_for_road_connection(matrix, road_one_groups, "E2"),
-        look_for_road_connection(matrix, road_two_groups, "E1"))
+    # gets all connections between Buildings and Warehouses (via Roads),
+    # also captures "dead-ends".
+    road_one_connections = look_for_road_connection(matrix, road_one_groups, "E2")
+    road_two_connections = look_for_road_connection(matrix, road_two_groups, "E1")
+    # separates and categorizes connections through Roads, Intersections and Dead Ends.
+    road_intersections, road_direct_connections, road_dead_ends = lookup_road_connections_deadends_intersections(road_one_connections, road_two_connections)
+    # assemble the graph with Graphviz dot code.
     build_graphviz_graph(building_ids, warehouse_ids, road_intersections, road_direct_connections, road_dead_ends)
+
 
 if __name__ == "__main__":
     main()
